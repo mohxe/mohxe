484ef4ca37c7d4b4adce72c1bf8995b639807d6e
Mohamed Ibrahima Taore <mohamed.github@protonmail.com>
2020-09-06 17:51:19 +0000

    Create App.svelte

---
+++ mohxe/resources/js/mohxespa/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router } from "svelte-router-spa";
+  import routes from "@routes";
+</script>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<Router {routes} />
