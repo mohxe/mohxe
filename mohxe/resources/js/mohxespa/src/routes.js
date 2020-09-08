e6ccd90350072b0b1d65641af67db23206c14ff7
Mohamed Ibrahima Taore <mohamed.github@protonmail.com>
2020-09-08 00:06:06 +0000

    feat(routing): Adding and renaming some routes
    Routes added:
    - 'blog'
    
    Route renamed:
    - 'a-propos' changed to 'about-us'

--- mohxe/resources/js/mohxespa/src/routes.js
+++ mohxe/resources/js/mohxespa/src/routes.js
@@ -2,14 +2,16 @@ import PublicLayout from '@views/public/layout.svelte';
 import PublicIndex from '@views/public/index.svelte';
 import About from '@views/public/about.svelte';
 import Contact from '@views/public/contact.svelte';
+import BlogIndex from '@views/public/blog/index.svelte';
 
 
 
 
 const routes = [
     { name: '/', component: PublicIndex, layout: PublicLayout },
-    { name: 'a-propos', component: About, layout: PublicLayout },
-    { name: 'contact', component: Contact, layout: PublicLayout }
+    { name: 'about-us', component: About, layout: PublicLayout },
+    { name: 'contact', component: Contact, layout: PublicLayout },
+    { name: 'blog', component: BlogIndex, layout: PublicLayout }
 ];
 
 export default routes;
