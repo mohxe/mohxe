ce56bc8d7b8160fd6695fa9698ea4dbce19610d8
Mohamed Ibrahima Taore <mohamed.github@protonmail.com>
2020-09-08 00:09:12 +0000

    Update the views/public/layout.svelte

--- mohxe/resources/js/mohxespa/src/views/public/layout.svelte
+++ mohxe/resources/js/mohxespa/src/views/public/layout.svelte
@@ -1,28 +1,14 @@
 <script>
-    import {Route} from 'svelte-router-spa'
-    
-    import NavigationBar from '@components/layout/navigation_bar.svelte';
+  import { Route } from "svelte-router-spa";
 
-    export let currentRoute;
-    export let params = {};
+  import Header from "@components/layout/header.svelte";
+
+  export let currentRoute;
+  export let params = {};
+
 </script>
-<header>
-    <!-- navigation -->
-    <nav>
-        <!-- logo -->
-        <div>
-            
-        </div>
 
-        <!-- links -->
-       <div>
-        <a href="/">link 1</a>
-        <a href="/">link 2</a>
-        <a href="/">link 2</a>
-       </div>
-        
-    </nav>
-</header>
-    <Route {currentRoute} {params}/>
-<footer>
-</footer>
+<Header {currentRoute} {params}/>
+
+<div class="pt-12"><Route {currentRoute} {params}/></div>
+<footer />
